6c8da6772bd31f8f67402d15cab88ea223ced8a3
gazzi <guy-azzi@hotmail.com>
2023-07-05 12:56:45 -0400

    initial commit

---
+++ src/test/java/com/resume/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
