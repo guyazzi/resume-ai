1f45f0b704b00142e8dd2bbe90eb6bc08b92cf19
gazzi <guy-azzi@hotmail.com>
2023-07-05 13:20:11 -0400

    controller + swagger

---
+++ src/main/java/com/resume/ai/controller/OAController.java
@@ -0,0 +1,16 @@
+package com.resume.ai.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@CrossOrigin
+@RestController
+@RequestMapping("/api/oa/v1")
+public class OAController {
+
+
+}
